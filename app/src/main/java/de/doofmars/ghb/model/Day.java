5ef1ea969be1774a5684799edb74ef9181b07255
doofmars <jan-preuss@gmx.de>
2015-03-17 22:45:24 +0100

    Added Getters, Equals and CompareTo

--- app/src/main/java/de/doofmars/ghb/model/Day.java
+++ app/src/main/java/de/doofmars/ghb/model/Day.java
@@ -4,7 +4,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Day {
+public class Day implements Comparable<Day>  {
     private final static SimpleDateFormat DF = new SimpleDateFormat("d.MM.yyyy");
     private long total;
     private long host_id;
@@ -27,6 +27,34 @@ public class Day {
         }
     }
 
+    public long getTotal() {
+        return total;
+    }
+
+    public long getHost_id() {
+        return host_id;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
     @Override
     public String toString() {
         return "Day{" +
@@ -38,5 +66,47 @@ public class Day {
                 ", year=" + year +
                 ", date=" + date +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Day day1 = (Day) o;
+
+        if (day != day1.day) return false;
+        if (host_id != day1.host_id) return false;
+        if (month != day1.month) return false;
+        if (total != day1.total) return false;
+        if (year != day1.year) return false;
+        if (date != null ? !date.equals(day1.date) : day1.date != null) return false;
+        if (hostname != null ? !hostname.equals(day1.hostname) : day1.hostname != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (total ^ (total >>> 32));
+        result = 31 * result + (int) (host_id ^ (host_id >>> 32));
+        result = 31 * result + (hostname != null ? hostname.hashCode() : 0);
+        result = 31 * result + day;
+        result = 31 * result + month;
+        result = 31 * result + year;
+        result = 31 * result + (date != null ? date.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public int compareTo(Day that) {
+        if (this.getDate().compareTo(that.getDate()) != 0) {
+            return this.getDate().compareTo(that.getDate());
+        }
+        if (this.getHostname().compareTo(that.getHostname()) != 0) {
+            return this.getHostname().compareTo(that.getHostname());
+        }
+        return 0;
     }
 }
