a1eabc3d71bd3b5dc3f3eb26f626c32d24b3a302
doofmars <jan-preuss@gmx.de>
2015-03-19 20:29:26 +0100

    Dataformatter to format for gb and mb and kb

--- app/src/main/java/de/doofmars/ghb/MyValueFormatter.java
+++ app/src/main/java/de/doofmars/ghb/MyValueFormatter.java
@@ -7,14 +7,23 @@ import java.text.DecimalFormat;
 public class MyValueFormatter implements ValueFormatter {
 
     private DecimalFormat mFormat;
-    
+    private final long KB_FACTOR = 1024;
+    private final long MB_FACTOR = 1048576;
+    private final long GB_FACTOR = 1073741824;
+
     public MyValueFormatter() {
-        mFormat = new DecimalFormat("###,###,###,##0.0");
+        mFormat = new DecimalFormat("#####.00");
     }
     
     @Override
     public String getFormattedValue(float value) {
-        return mFormat.format(value) + " $";
+        if (value > GB_FACTOR) {
+            return mFormat.format(value / GB_FACTOR) + " GB";
+        } else if (value > MB_FACTOR) {
+            return mFormat.format(value / MB_FACTOR) + " MB";
+        } else {
+            return mFormat.format(value / KB_FACTOR) + " KB";
+        }
     }
 
 }
