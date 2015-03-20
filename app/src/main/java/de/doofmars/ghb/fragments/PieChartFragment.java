79451e6caf8b7c6a28f8d13ac9820d9f6a7a807c
doofmars <jan-preuss@gmx.de>
2015-03-20 21:35:55 +0100

    Working bar chart

--- app/src/main/java/de/doofmars/ghb/fragments/PieChartFragment.java
+++ app/src/main/java/de/doofmars/ghb/fragments/PieChartFragment.java
@@ -1,15 +1,12 @@
 package de.doofmars.ghb.fragments;
 
-import android.support.v4.app.Fragment;
-import android.graphics.Typeface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.SeekBar;
-import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
@@ -24,22 +21,16 @@ import com.github.mikephil.charting.utils.PercentFormatter;
 import java.util.ArrayList;
 
 import de.doofmars.ghb.R;
+import de.doofmars.ghb.model.TrafficReport;
+import de.doofmars.ghb.util.CustomValueFormatter;
 
 /**
  * Created by Jan on 20.03.2015.
  */
-public class PieChartFragment extends Fragment implements SeekBar.OnSeekBarChangeListener,
-        OnChartValueSelectedListener {
+public class PieChartFragment extends Fragment implements OnChartValueSelectedListener {
 
     private PieChart mChart;
-    private SeekBar mSeekBarX, mSeekBarY;
-    private TextView tvX, tvY;
-    protected String[] mParties = new String[] {
-            "Party A", "Party B", "Party C", "Party D", "Party E", "Party F", "Party G", "Party H",
-            "Party I", "Party J", "Party K", "Party L", "Party M", "Party N", "Party O", "Party P",
-            "Party Q", "Party R", "Party S", "Party T", "Party U", "Party V", "Party W", "Party X",
-            "Party Y", "Party Z"
-    };
+    private TrafficReport report;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -47,35 +38,26 @@ public class PieChartFragment extends Fragment implements SeekBar.OnSeekBarChang
 
         View rootView = inflater.inflate(R.layout.fragment_piechart, container, false);
 
-        tvX = (TextView) rootView.findViewById(R.id.tvXMax);
-        tvY = (TextView) rootView.findViewById(R.id.tvYMax);
-        mSeekBarX = (SeekBar) rootView.findViewById(R.id.seekBar1);
-        mSeekBarY = (SeekBar) rootView.findViewById(R.id.seekBar2);
-        mSeekBarY.setProgress(10);
-        mSeekBarX.setOnSeekBarChangeListener(this);
-        mSeekBarY.setOnSeekBarChangeListener(this);
         mChart = (PieChart) rootView.findViewById(R.id.chart1);
         mChart.setUsePercentValues(true);
-// change the color of the center-hole
-// mChart.setHoleColor(Color.rgb(235, 235, 235));
         mChart.setHoleColorTransparent(true);
-
-        mChart.setHoleRadius(60f);
+        mChart.setHoleRadius(50f);
         mChart.setDescription("");
         mChart.setDrawCenterText(true);
         mChart.setDrawHoleEnabled(true);
-        mChart.setRotationAngle(0);
-// enable rotation of the chart by touch
-        mChart.setRotationEnabled(true);
-// mChart.setUnit(" €");
-// mChart.setDrawUnitsInChart(true);
-// add a selection listener
+        mChart.setRotationAngle(270);
+        mChart.setRotationEnabled(false);
         mChart.setOnChartValueSelectedListener(this);
-// mChart.setTouchEnabled(false);
-        mChart.setCenterText("MPAndroidChart\nLibrary");
-        setData(3, 100);
+        mChart.setCenterText(getResources().getString(R.string.pie_chart_center_label));
+
+        //Load the data
+        Intent intent = getActivity().getIntent();
+        report = (TrafficReport) intent.getSerializableExtra(getResources().getString(R.string.traffic_report_key));
+        setData();
+
         mChart.animateXY(1500, 1500);
 // mChart.spin(2000, 0, 360);
+
         Legend l = mChart.getLegend();
         l.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);
         l.setXEntrySpace(7f);
@@ -84,28 +66,18 @@ public class PieChartFragment extends Fragment implements SeekBar.OnSeekBarChang
         return rootView;
     }
 
-    @Override
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        tvX.setText("" + (mSeekBarX.getProgress() + 1));
-        tvY.setText("" + (mSeekBarY.getProgress()));
-        setData(mSeekBarX.getProgress(), mSeekBarY.getProgress());
-    }
+   private void setData() {
+        ArrayList<Entry> yVals1 = new ArrayList<>();
+        ArrayList<String> xVals = report.getHosts();
 
-    private void setData(int count, float range) {
-        float mult = range;
-        ArrayList<Entry> yVals1 = new ArrayList<Entry>();
-// IMPORTANT: In a PieChart, no values (Entry) should have the same
-// xIndex (even if from different DataSets), since no values can be
-// drawn above each other.
-        for (int i = 0; i < count + 1; i++) {
-            yVals1.add(new Entry((float) (Math.random() * mult) + mult / 5, i));
+        for (int i = 0; i < xVals.size(); i++) {
+            yVals1.add(new Entry(report.getTrafficByHost(xVals.get(i)), i));
         }
-        ArrayList<String> xVals = new ArrayList<String>();
-        for (int i = 0; i < count + 1; i++)
-            xVals.add(mParties[i % mParties.length]);
-        PieDataSet dataSet = new PieDataSet(yVals1, "Election Results");
+
+        PieDataSet dataSet = new PieDataSet(yVals1, getResources().getString(R.string.pie_chart_legend_label));
         dataSet.setSliceSpace(3f);
-// add a lot of colors
+
+        // add a lot of colors
         ArrayList<Integer> colors = new ArrayList<Integer>();
         for (int c : ColorTemplate.VORDIPLOM_COLORS)
             colors.add(c);
@@ -119,6 +91,7 @@ public class PieChartFragment extends Fragment implements SeekBar.OnSeekBarChang
             colors.add(c);
         colors.add(ColorTemplate.getHoloBlue());
         dataSet.setColors(colors);
+
         PieData data = new PieData(xVals, dataSet);
         data.setValueFormatter(new PercentFormatter());
         data.setValueTextSize(11f);
@@ -132,64 +105,12 @@ public class PieChartFragment extends Fragment implements SeekBar.OnSeekBarChang
     public void onValueSelected(Entry e, int dataSetIndex, Highlight h) {
         if (e == null)
             return;
-        Log.i("VAL SELECTED",
-                "Value: " + e.getVal() + ", xIndex: " + e.getXIndex()
-                        + ", DataSet index: " + dataSetIndex);
+        mChart.setCenterText(report.getHost(e.getXIndex()) + "\n" +new CustomValueFormatter().getFormattedValue(e.getVal()));
     }
 
     @Override
     public void onNothingSelected() {
-        Log.i("PieChart", "nothing selected");
+        mChart.setCenterText(getResources().getString(R.string.pie_chart_center_label));
     }
 
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
-// TODO Auto-generated method stub
-    }
-
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
-// TODO Auto-generated method stub
-    }
-// private void removeLastEntry() {
-//
-// PieData data = mChart.getDataOriginal();
-//
-// if (data != null) {
-//
-// PieDataSet set = data.getDataSet();
-//
-// if (set != null) {
-//
-// Entry e = set.getEntryForXIndex(set.getEntryCount() - 1);
-//
-// data.removeEntry(e, 0);
-// // or remove by index
-// // mData.removeEntry(xIndex, dataSetIndex);
-//
-// mChart.notifyDataSetChanged();
-// mChart.invalidate();
-// }
-// }
-// }
-//
-// private void addEntry() {
-//
-// PieData data = mChart.getDataOriginal();
-//
-// if (data != null) {
-//
-// PieDataSet set = data.getDataSet();
-// // set.addEntry(...);
-//
-// data.addEntry(new Entry((float) (Math.random() * 25) + 20f,
-// set.getEntryCount()), 0);
-//
-// // let the chart know it's data has changed
-// mChart.notifyDataSetChanged();
-//
-// // redraw the chart
-// mChart.invalidate();
-// }
-// }
 }
